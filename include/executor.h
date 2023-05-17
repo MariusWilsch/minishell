dd9728fbf7548ebffd65a18ada3ff21f87d7f999
tklouwer <tklouwer@student.codam.nl>
2023-05-17 16:24:05 +0200

    all

--- include/executor.h
+++ include/executor.h
@@ -6,7 +6,7 @@
 /*   By: verdant <verdant@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/24 11:18:31 by mwilsch       #+#    #+#                 */
-/*   Updated: 2023/05/17 16:17:11 by tklouwer      ########   odam.nl         */
+/*   Updated: 2023/05/17 16:23:38 by tklouwer      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,7 @@ int		exisit_env(t_env **env_list, char *str, t_env **found);
 void	shell_exit(t_cmds *cmd);
 
 /* 				UTILS			 */
+bool	check_flag(char *argv[]);
 int		count_args(char **argv);
 void	execute_command(t_cmds *head);
 int		p_error(char *str, int status);
