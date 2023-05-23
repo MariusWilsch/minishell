f27657974d16e166cd39e36d277b1797abf46740
tklouwer <tklouwer@student.codam.nl>
2023-05-23 14:40:55 +0200

    EVERYTHING BUT PIPES WORKING

--- src/executor/execute.c
+++ src/executor/execute.c
@@ -6,13 +6,52 @@
 /*   By: mwilsch <mwilsch@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/22 16:40:17 by tklouwer      #+#    #+#                 */
-/*   Updated: 2023/05/23 12:56:52 by tklouwer      ########   odam.nl         */
+/*   Updated: 2023/05/23 14:39:39 by tklouwer      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 #include <sys/wait.h>
 
+
+void close_pipes_new(int *pipe_fd, int cmd_cnt, int current_cmd)
+{
+    int i;
+
+    i = 0;
+    while (i < 2 * cmd_cnt)
+    {
+        // Close read end of the current pipe and write end of the next pipe
+        if (i != 2 * current_cmd && i != 2 * current_cmd + 1)
+            if (close(pipe_fd[i]) < 0)
+                p_error("close", EXIT_FAILURE);
+        i++;
+    }
+}
+
+void	create_process(t_cmds *cmd, int cmd_cnt, int *pipe_fd, pid_t *pid)
+{
+	int	i;
+
+	i = 0;
+	while (i < cmd_cnt)
+	{
+		if (cmd[i].cmd_type == BUILT_IN_EXE
+			&& !(ft_strncmp("echo", cmd[i].cmd_path, 4) == 0))
+			exec_builtin(cmd->cmd_path, cmd->argc, cmd->argv, cmd->env);
+		else
+		{
+			pid[i] = fork();
+			if (pid[i] < 0)
+				p_error("fork", 1);
+			else if (pid[i] == 0)
+			{
+				child_process(cmd, i, cmd_cnt, pipe_fd);
+			}
+		}
+		i++;
+	}
+}
 
 /* EXECUTES THE ACTIONS THAT NEEDS TO BE PERFORMED FOR THE PARENT PROCESS. 
 	WAITS TILL THE CHILD PROCESS IS FINISHED EXECUTING, CLOSES THE FD'S.
@@ -49,33 +88,10 @@ int	child_process(t_cmds *cmd, int i, int cmd_cnt, int *pipe_fd)
 	close_pipes(pipe_fd, cmd_cnt, i, 0);
 	if (cmd_cnt != 1 && !cmd->redir)
 		redirect_pipe_fd(i, cmd_cnt, pipe_fd);
-	process_redirection(cmd, i, pipe_fd);
+	if (cmd->redir)
+		process_redirection(cmd, i, pipe_fd);
 	execute_cmd_or_builtin(cmd);
 	exit(EXIT_SUCCESS);
-}
-
-void	create_process(t_cmds *cmd, int cmd_cnt, int *pipe_fd, pid_t *pid)
-{
-	int	i;
-
-	i = 0;
-	while (i < cmd_cnt)
-	{
-		if (cmd[i].cmd_type == BUILT_IN_EXE
-			&& !(ft_strncmp("echo", cmd[i].cmd_path, 4) == 0))
-			exec_builtin(cmd->cmd_path, cmd->argc, cmd->argv, cmd->env);
-		else
-		{
-			pid[i] = fork();
-			if (pid[i] < 0)
-				p_error("fork", 1);
-			else if (pid[i] == 0)
-			{
-				child_process(cmd, i, cmd_cnt, pipe_fd);
-			}
-		}
-		i++;
-	}
 }
 
 void	shell_process(t_cmds *cmd, int cmd_cnt, int *pipe_fd)
--- src/executor/redir_io.c
+++ src/executor/redir_io.c
@@ -6,7 +6,7 @@
 /*   By: verdant <verdant@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/29 16:26:30 by tklouwer      #+#    #+#                 */
-/*   Updated: 2023/05/23 12:56:45 by tklouwer      ########   odam.nl         */
+/*   Updated: 2023/05/23 14:40:31 by tklouwer      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	redirect_pipe_fd(int i, int cmd_cnt, int *pipe_fd)
 			p_error("dup2", EXIT_FAILURE);
 		return (close(pipe_fd[2 * i + 1]));
 	}
-	else if (i < cmd_cnt - 1)
+	if (i < cmd_cnt - 1)
 	{
 		if (dup2(pipe_fd[2 * (i - 1)], STDIN_FILENO) == -1)
 			p_error("dup2", EXIT_FAILURE);
