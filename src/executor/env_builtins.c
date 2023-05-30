ed8b938b88733536f5e4eef53164d0e202fcba2e
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2023-05-30 16:32:47 +0100

    Continued with export

--- src/executor/env_builtins.c
+++ src/executor/env_builtins.c
@@ -6,7 +6,7 @@
 /*   By: verdant <verdant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 10:40:09 by tklouwer          #+#    #+#             */
-/*   Updated: 2023/05/30 16:01:47 by verdant          ###   ########.fr       */
+/*   Updated: 2023/05/30 16:30:54 by verdant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,56 +55,50 @@ int	exisit_env(t_env **env_list, char *str, t_env **found)
 	return (-1);
 }
 
-// int	export_checks(char *str)
-// {
-	
-// }
+void	export_routine(char *str, t_env **env_list)
+{
+	t_env	*found;
+
+	found = NULL;
+	if (ft_strchr(str, '=') == NULL && exisit_env(env_list, str, &found) == -1)
+	{
+		add_end(env_list, str, true);
+		return ;
+	}
+	exisit_env(env_list, str, &found);
+	if (found == NULL)
+		add_end(env_list, str, false);
+	if (found != NULL)
+	{
+		free(found->value);
+		found->value = ft_strdup(str + ft_strlen(found->key) + 1);
+		if (found->export_only == true)
+			found->export_only = false;
+	}
+}
 
 
 int	export(int argc, char *argv[], t_env **env_list)
 {
 	int		i;
-	int		k;
-	t_env	*found;
-	t_env	*temp;
+	int		j;
 
 	i = 1;
 	if (argc == 1)
 		return (env(env_list, true));
 	while (i < argc)
 	{
-		if (ft_strchr(argv[i], '=') == NULL && exisit_env(env_list, argv[i], &temp) == -1)
+		j = 0;
+		while (argv[i][j] != '\0' && argv[i][j] != '=')
 		{
-			k = 0;
-			while (argv[i][k] && ft_isalnum(argv[i][k]))
-				k++;
-			if (argv[i][k] == '\0')
+			if (ft_isalnum(argv[i][j]) == 0 && ft_isdigit(argv[i][0]) == 0)
 			{
-				add_end(env_list, argv[i], true);
-				i++;
-				continue ;
+				ft_printf("minishell: export: %s not a valid identifier\n", argv[i]);
+				return (g_status = EXIT_FAILURE);
 			}
+			j++;
 		}
-		if (ft_strchr(argv[i], '=') == NULL)
-		{
-			i++;
-			continue ;
-		}
-		if (ft_isdigit(argv[i][0]))
-		{
-			ft_printf("minishell: export: not a valid identifier\n", argv[i]);
-			return (g_status = EXIT_FAILURE);
-		}
-		exisit_env(env_list, argv[i], &found);
-		if (found == NULL)
-			add_end(env_list, argv[i], false);
-		if (found != NULL)
-		{
-			free(found->value);
-			found->value = ft_strdup(argv[i] + ft_strlen(found->key) + 1);
-			if (found->export_only == true)
-				found->export_only = false;
-		}
+		export_routine(argv[i], env_list);
 		i++;
 	}
 	return (g_status = EXIT_SUCCESS);
